2ebf906e6e094a4370ac665ae52fb85080f3ed86
Shubhamabhimanyu <sg40180@gmail.com>
2022-04-01 23:28:56 +0530

    commit

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -27,9 +27,7 @@ export default function Navbar() {
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid">
         <a className="navbar-brand" href='/'>TODO </a>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
+       
         <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
           <div className="navbar-nav">
             
